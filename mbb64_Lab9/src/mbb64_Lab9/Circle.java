19a102217769df00a75abda1fad973f9f15e087b
mbb64 <mbb64@pitt.edu>
2022-11-14 18:36:59 -0500

    lab9

--- mbb64_Lab9/src/mbb64_Lab9/Circle.java
+++ mbb64_Lab9/src/mbb64_Lab9/Circle.java
@@ -1,5 +1,43 @@
 package mbb64_Lab9;
 
 public class Circle {
+	private double radius;
+	private String color;
+
+	public Circle() {
+		this.radius = 5.0;
+		this.color = "Green";
+	}
+
+	public Circle(double radius) {
+		this.radius = radius;
+		this.color = "Green";
+	}
+
+	public Circle(double radius, String color) {
+		this.radius = radius;
+		this.color = color;
+	}
+
+	public double getRadius() {
+		return this.radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	public String getColor() {
+		return this.color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public double getArea() {
+		return (Math.PI * radius * radius);
+
+	}
 
 }
